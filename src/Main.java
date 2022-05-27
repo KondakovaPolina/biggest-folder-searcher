e1187ea280effb7cdd9eb3b8865809ada5898af6
Polina Kondakova <foxypollya@gmail.com>
2022-05-27 22:03:58 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
-        String folderPath = "C:/Users/user/Desktop/Photo";
+
+        String folderPath = "C:/Users/user/Desktop";
         File file = new File(folderPath);
 
-        System.out.println(getFolderSize(file));
+        long start = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+
+        long duration = System.currentTimeMillis() - start; //время в милисекундах
+        System.out.println(duration + " ms");
     }
 
     public static long getFolderSize(File folder) {
